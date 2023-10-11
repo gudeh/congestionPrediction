eb954e240b63492d22373819addfba629eedc708
gudeh <gudehh@gmail.com>
2023-10-11 18:14:35 -0300

    update regular exp

--- downloadArvoreNatal.sh
+++ downloadArvoreNatal.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-remote_host="ecl@150.162.57.???"
+remote_host="ecl@150.162.57.235"
 remote_dir="~/1augusto/"
 local_dir="download"
 
 # Execute rsync command to download files from remote directory
-rsync -avz --exclude='dataSet' --exclude='c17' --exclude='gcd' --exclude='backup' -e ssh "$remote_host:$remote_dir" "$local_dir"
+#rsync -avz --exclude='nangate' --exclude='dataSet' --exclude='c17' --exclude='gcd' --exclude='backup' --exclude='asap7' -e ssh "$remote_host:$remote_dir" "$local_dir"
+rsync -avz --exclude='*nangate*' --exclude='dataSet' --exclude='c17' --exclude='gcd' --exclude='backup' --exclude='asap7' -e ssh "$remote_host:$remote_dir" "$local_dir"
+
 
