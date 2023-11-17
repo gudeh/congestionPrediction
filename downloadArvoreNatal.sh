7f78f79e96e0626f3bf0854dfa402a82be32e960
gudeh <gudehh@gmail.com>
2023-11-17 10:21:59 -0300

    update!

--- downloadArvoreNatal.sh
+++ downloadArvoreNatal.sh
@@ -8,6 +8,6 @@ rm -rf $local_dir
 
 # Execute rsync command to download files from remote directory
 #rsync -avz --exclude='nangate' --exclude='dataSet' --exclude='c17' --exclude='gcd' --exclude='backup' --exclude='asap7' -e ssh "$remote_host:$remote_dir" "$local_dir"
-rsync -avz --exclude='nangate-STDfeatures-missing-bpQuad-memPool' --exclude='nangate'  --exclude='dataSet' --exclude='c17' --exclude='backup' --exclude='asap7' -e ssh "$remote_host:$remote_dir" "$local_dir"
+rsync -avz --exclude='nangateV1+closeness+between' --exclude='asap7V1+closeness+between' --exclude='.git' --exclude='asap7V0+closeness+between' --exclude='nangate-STDfeatures-missing-bpQuad-memPool' --exclude='nangate'  --exclude='dataSet' --exclude='c17' --exclude='backup' --exclude='asap7' --exclude='asap7-stdCell' -e ssh "$remote_host:$remote_dir" "$local_dir"
 
 
